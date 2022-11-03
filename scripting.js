125fcb14228edeb42a63e1a2fa5e028d76b25671
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-11-03 13:24:36 -0700

    codepen

--- scripting.js
+++ scripting.js
@@ -1,55 +1,106 @@
 function wrapErrorAlert(fn, name) {
-    return function(...args) {
+    return function (...args) {
         try {
-            fn(...args)
+            fn(...args);
+        } catch (e) {
+            return
+            alert(
+                `Error!\n${e}\ncalled with ${args}\n${
+                    name == undefined ? "No specified name" : "Name: " + name
+                }`
+            );
         }
-        catch (e) {
-            alert(`Error!\n${e}\ncalled with ${args}\n${name==undefined?"No specified name":"Name: " + name}`)
-        }
-    }
+    };
 }
 
-window.addEventListener("load", wrapErrorAlert(() => {
-    const left = document.querySelector(".left");
-    const right = document.querySelector(".right");
-    const container = document.querySelector("body");
-    left.addEventListener("mouseenter", () => {
-        container.classList.add("hover-left");
-    });
-    left.addEventListener("mouseleave", () => {
-        container.classList.remove("hover-left");
-    });
-    right.addEventListener("mouseenter", () => {
-        container.classList.add("hover-right");
-    });
-    right.addEventListener("mouseleave", () => {
-        container.classList.remove("hover-right");
-    });
+window.addEventListener(
+    "load",
+    wrapErrorAlert(() => {
+        const left = document.querySelector(".left");
+        const leftO = {
+            score: left.querySelector(".score"),
+            red: left.querySelector(".red"),
+            green: left.querySelector(".green"),
+            blue: left.querySelector(".blue"),
+            purity: left.querySelector(".purity"),
+            clarity: left.querySelector(".clarity")
+        };
+        const right = document.querySelector(".right");
+        const rightO = {
+            score: right.querySelector(".score"),
+            red: right.querySelector(".red"),
+            green: right.querySelector(".green"),
+            blue: right.querySelector(".blue"),
+            purity: right.querySelector(".purity"),
+            clarity: right.querySelector(".clarity")
+        };
+        const container = document.querySelector("body");
+        left.addEventListener("mouseenter", () => {
+            container.classList.add("hover-left");
+        });
+        left.addEventListener("mouseleave", () => {
+            container.classList.remove("hover-left");
+        });
+        right.addEventListener("mouseenter", () => {
+            container.classList.add("hover-right");
+        });
+        right.addEventListener("mouseleave", () => {
+            container.classList.remove("hover-right");
+        });
 
-    function scoreColor(rgb) {
-        let r = rgb[0];
-        let g = rgb[1];
-        let b = rgb[2];
-        const purity = g / (r + g + b);
-        const clarity = Math.min((g/255) * 2, 1);
-        return purity * clarity;
-    }
+        function scoreColor(rgb) {
+            let r = rgb[0];
+            let g = rgb[1];
+            let b = rgb[2];
+            const purity = g / (r + g + b);
+            const clarity = Math.min((g / 255) * 4, 1);
+            return [purity * clarity, purity, clarity];
+        }
 
-    function generateRandomColor() {
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-        return [r, g, b];
-    }
-    let leftCol = generateRandomColor();
-    let rightCol = generateRandomColor();
+        function generateRandomColor() {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            return [r, g, b];
+        }
+        let leftCol = generateRandomColor();
+        let rightCol = generateRandomColor();
 
-    function generateColorString(rgb) {
-        let r = rgb[0];
-        let g = rgb[1];
-        let b = rgb[2];
-        return `rgb(${r}, ${g}, ${b})`
-    }
-    left.style.backgroundColor = generateColorString(leftCol);
-    right.style.backgroundColor = generateColorString(rightCol);
-}))
+        function generateColorString(rgb) {
+            let r = rgb[0];
+            let g = rgb[1];
+            let b = rgb[2];
+            return `rgb(${r}, ${g}, ${b})`;
+        }
+        
+        function updColors() {
+            let sl = scoreColor(leftCol);
+            let sr = scoreColor(rightCol)
+            left.style.backgroundColor = generateColorString(leftCol);
+            right.style.backgroundColor = generateColorString(rightCol);
+            leftO.score.innerHTML = Math.floor(sl[0] * 1000)/10 + "%";
+            rightO.score.innerHTML = Math.floor(sr[0] * 1000)/10 + "%";
+            leftO.red.innerHTML = leftCol[0];
+            leftO.green.innerHTML = leftCol[1];
+            leftO.blue.innerHTML = leftCol[2];
+            rightO.red.innerHTML = rightCol[0];
+            rightO.green.innerHTML = rightCol[1];
+            rightO.blue.innerHTML = rightCol[2];
+            
+            leftO.purity.innerHTML = Math.floor(sl[1] * 1000)/10 + "%";
+            rightO.purity.innerHTML = Math.floor(sr[1] * 1000)/10 + "%";
+            
+            leftO.clarity.innerHTML = Math.floor(sl[2] * 1000)/10 + "%";
+            rightO.clarity.innerHTML = Math.floor(sr[2] * 1000)/10 + "%";
+        }
+
+        updColors();
+        
+        setInterval(() => {
+            
+            leftCol = generateRandomColor();
+            rightCol = generateRandomColor();
+            updColors();
+        }, 1000)
+    }, "window load listener")
+);
