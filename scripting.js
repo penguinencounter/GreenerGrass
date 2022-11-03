c32c901f6d7f99ad76d76f9ccd5f7dd2c76cd38d
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-11-03 14:55:42 -0700

    the purification

--- scripting.js
+++ scripting.js
@@ -228,7 +228,7 @@ window.addEventListener(
         setInterval(() => {
             // Clarity above 50%;
             let f = [
-                filterClarity(0, 0.1),
+                filterClarity(0.9, 1),
                 filterPurity(0.9, 1)
             ]
 
