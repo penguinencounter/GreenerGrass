76bdc03275c6c0a3ac606a89dfb96d891a01c786
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-11-03 13:38:24 -0700

    what

--- scripting.js
+++ scripting.js
@@ -109,7 +109,7 @@ window.addEventListener(
             // Clarity above 50%;
             let f = c => {
                 let s = scoreColor(c)
-                return s[0] > 0.95;
+                return s[0] > 0.95 && s[2] < 1;
             }
 
             leftCol = generateLimitedColor(f);
